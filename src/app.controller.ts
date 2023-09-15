61ff32d1e93175ba0f93915a3a39f0eed0e3f024
Shameel Uddin <shameel_uddin@yahoo.com>
2023-09-15 16:08:08 +0500

    Basic Endpoints Created and Tested on Postman.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
-@Controller()
+@Controller('report/:type')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllReports(@Param() params): string[] {
+    console.log(params);
+    return [];
+  }
+
+  @Get(':id')
+  getReportById(): any {
+    return 'Report by id';
+  }
+
+  @Post()
+  createReport() {
+    return 'Report created';
+  }
+
+  @Put(':id')
+  updateReport() {
+    return 'Report updated';
+  }
+
+  @Delete(':id')
+  deleteReport() {
+    return 'Report deleted';
   }
 }
