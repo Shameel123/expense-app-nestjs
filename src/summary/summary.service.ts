d9562bb797bde75215aded426cd5da0fabcca0fd
Shameel Uddin <shameel_uddin@yahoo.com>
2023-09-16 15:09:02 +0500

    Expense App Completed!

--- src/summary/summary.service.ts
+++ src/summary/summary.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { REPORT_TYPE } from 'src/data';
+import { ReportService } from 'src/report/report.service';
 
 @Injectable()
-export class SummaryService {}
+export class SummaryService {
+  constructor(private readonly reportService: ReportService) {}
+  calculateSummary() {
+    const totalIncome = this.reportService
+      .getAllReports(REPORT_TYPE.INCOME)
+      .map((report) => report.amount)
+      .reduce((a, b) => a + b, 0);
+    const totalExpense = this.reportService
+      .getAllReports(REPORT_TYPE.EXPENSE)
+      .map((report) => report.amount)
+      .reduce((a, b) => a + b, 0);
+    const netIncome = totalIncome - totalExpense;
+
+    return {
+      totalIncome,
+      totalExpense,
+      netIncome,
+    };
+  }
+}
